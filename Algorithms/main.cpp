98bf4e97c4be13379d7e8e4fd8fc4ce2683e6ed1
Peter Revel <peter.revel@gmail.com>
2014-06-24 22:01:30 -0700

    Initial Commit

---
+++ Algorithms/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Algorithms
+//
+//  Created by Hawthorne Labs on 6/24/14.
+//  Copyright (c) 2014 Peter Reveles. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
